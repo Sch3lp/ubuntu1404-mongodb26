20ce57b7377977b84ead66749054bd51fc40117f
Sch3lp <sch3lp@gmail.com>
2015-03-14 16:08:06 +0100

    Forgot to actually call the create and populate scripts...

--- init_mongodb.sh
+++ init_mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 
 echo "Initializing MongoDB instance w/ Fiazards Collections and initial data"
-ls -lah /opt/fiazard/conf
+mongo /opt/fiazard/conf/scripts/01_createTables.js
+mongo /opt/fiazard/conf/scripts/02_insertRefData.js
